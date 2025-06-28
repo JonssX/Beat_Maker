001d206ff281770d6cc50dae74738f6d4819c002
JonssX <luk.rzaca@gmail.com>
2025-06-28 12:20:07 +0200

    commit

--- app.js
+++ app.js
@@ -13,5 +13,17 @@ const kickInstrument = document.getElementById("kick_instrument");
 
 kickInstrument.addEventListener("click", () => {
   kickOneSound.play();
-  console.log("played");
 });
+
+// Jak nałożyć granie na każdy z elementów?
+// const beatBox = document.getElementsByClassName("beat_box");
+
+// const iterate = () => {
+//   for (let i; i < beatBox.length; i++) {
+//     beatBox[i].addEventListener("click", () => {
+//       kickOneSound.play();
+//     });
+//   }
+// };
+
+// iterate();
